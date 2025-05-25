3049dfe50222dae144f4ccaef83b2924cf5ef2c8
makjn10 <10.mayank.jain@gmail.com>
2025-05-25 22:06:43 +0530

    First commit

---
+++ src/main/java/com/mayank/SpringBootPractice/controllers/MyController.java
@@ -0,0 +1,21 @@
+package com.mayank.SpringBootPractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demoApp/")
+public class MyController {
+
+    @GetMapping("/")
+    public String mapping1() {
+        return "Welcome to Demo App";
+    }
+
+    @GetMapping("/api1")
+    public String mapping2() {
+        return "Welcome to Demo App API 1";
+    }
+}
